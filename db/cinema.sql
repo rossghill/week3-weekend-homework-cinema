bea09ce93339b8303b98fee21987755dd6592725
Ross Hill <rosshill88@gmail.com>
2017-12-08 11:41:44 +0000

    db/tables tested and working in psql

--- db/cinema.sql
+++ db/cinema.sql
@@ -7,16 +7,16 @@ CREATE TABLE customers (
 id SERIAL4 PRIMARY KEY,
 name VARCHAR(255),
 funds INT4
-)
+);
 
 CREATE TABLE films (
 id SERIAL4 PRIMARY KEY,
 title VARCHAR(255),
 price INT4
-)
+);
 
 CREATE TABLE tickets(
 id SERIAL4 PRIMARY KEY,
-customer_id INT4 REFERENCES customers(id)
+customer_id INT4 REFERENCES customers(id),
 film_id INT4 REFERENCES films(id)
-)
+);
